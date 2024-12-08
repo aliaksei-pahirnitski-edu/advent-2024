e212e07396fa9c5954a1fdbd1b8a1990c49cdaff
aliaksei.pahirnitski.edu <aliaksei.pahirnitski.edu@gmail.com>
2024-12-08 09:59:26 +0300

    puzzle 01_2

--- 01/HistorianConsole/Program.cs
+++ 01/HistorianConsole/Program.cs
@@ -3,12 +3,18 @@ Console.WriteLine("Hello, World!");
 
 //int[] list1 = [3, 4, 2, 1, 3, 3];
 //int[] list2 = [4, 3, 5, 3, 9, 3];
-// var input = ReadInput();
 var (list1, list2) = ReadInput();
 
-var result = GetTotalDistance(list1, list2);
+// puzzle 1  // 2580760
+// var result = GetTotalDistance(list1, list2);
 
-Console.WriteLine(result);   // 2580760
+// puzzle 2  // 25358365
+var result = GetSimilarityScope(list1, list2);
+Console.WriteLine(result);  
+
+int GetSimilarityScope(IReadOnlyList<int> first, IReadOnlyList<int> second) =>
+    first.Select(x => x * second.Count(y => y ==x))
+    .Sum();
 
 int GetTotalDistance(IReadOnlyList<int> first, IReadOnlyList<int> second) =>
     first.OrderBy(x => x).Zip(second.OrderBy(x => x))
