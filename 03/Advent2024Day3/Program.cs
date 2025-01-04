e0daceb39ab8fd620a2a9e0b1c29f7824b21a98a
aliaksei.pahirnitski.edu <aliaksei.pahirnitski.edu@gmail.com>
2025-01-04 13:11:11 +0300

    Start Day 3

---
+++ 03/Advent2024Day3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Advent2024Day3;
+
+Console.WriteLine("Hello, World!");
+
+var result = SolutionDay3Part1.Solve(PuzzleInput.Instructions);
+
+Console.WriteLine("Mull result = " + result);
---
+++ 03/Advent2024Day3/SolutionDay3Part1.cs
@@ -0,0 +1,9 @@
+﻿namespace Advent2024Day3;
+
+internal static class SolutionDay3Part1
+{
+    public static int Solve(string puzzleInput)
+    {
+        return 2;
+    }
+}
